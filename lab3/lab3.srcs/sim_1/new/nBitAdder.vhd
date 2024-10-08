e60bb319eaf78a9375b54fb4675fffeab055f0c2
Brandon Frazier <frazierb2@vcu.edu>
2024-10-08 09:47:06 -0400

    nbitadder tb addition

---
+++ lab3/lab3.srcs/sim_1/new/nBitAdder.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/08/2024 09:10:16 AM
+-- Design Name: 
+-- Module Name: nBitAdder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity nBitAdder is
+--  Port ( );
+end nBitAdder;
+
+architecture Behavioral of nBitAdder is
+
+begin
+
+
+end Behavioral;
